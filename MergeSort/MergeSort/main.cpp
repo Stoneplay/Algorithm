272e1174e74690a36fbe9162a7a9cb5fb278d443
Stoneplay <934916760@qq.com>
2019-03-23 13:15:47 +0800

    归并排序

--- MergeSort/MergeSort/main.cpp
+++ MergeSort/MergeSort/main.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-void Merge(vector<int>& nums, int left, int mid, int right, vector<int>& tmp);
-void Msort(vector<int>& nums, int left, int right, vector<int>& tmp);
+void Merge(vector<int>& nums, int left, int mid, int right);
+void Msort(vector<int>& nums, int left, int right);
 void MergeSort(vector<int>& nums);
 
 int main() {
@@ -30,8 +30,9 @@ int main() {
 }
 
 /* 合并两个有序数组 */
-void Merge(vector<int>& nums, int left, int mid, int right, vector<int>& tmp) {
+void Merge(vector<int>& nums, int left, int mid, int right) {
 	int i = left, j = mid + 1;
+	vector<int> tmp;
 
 	while (i <= mid && j <= right) {
 		if (nums[i] < nums[j]) {
@@ -52,22 +53,21 @@ void Merge(vector<int>& nums, int left, int mid, int right, vector<int>& tmp) {
 	for (size_t i = 0; i < tmp.size(); i++) {
 		nums[left + i] = tmp[i];	// 用已排序的元素覆盖原数组中未排序的元素
 	}
-	tmp.clear();	// 清空tmp,用于下一次合并操作
 }
 
 /* 将待排序数组分为两个子数组,分别排序后再合并 */
-void Msort(vector<int>& nums, int left, int right, vector<int>& tmp) {
+void Msort(vector<int>& nums, int left, int right) {
 	if (left < right) {
 		int mid = left + ((right - left) >> 2);
-		Msort(nums, left, mid, tmp);
-		Msort(nums, mid + 1, right, tmp);
-		Merge(nums, left, mid, right, tmp);
+		Msort(nums, left, mid);
+		Msort(nums, mid + 1, right);
+		Merge(nums, left, mid, right);
 	}
 }
 
 void MergeSort(vector<int>& nums) {
 	if (nums.size() < 2) return;
-	
+
 	vector<int> tmp;
-	Msort(nums, 0, nums.size() - 1, tmp);
+	Msort(nums, 0, nums.size() - 1);
 }
